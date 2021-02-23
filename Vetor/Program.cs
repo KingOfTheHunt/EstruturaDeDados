b7a63732708314c234648542e15067bf98135ad6
Davi <davifrancisco214@outlook.com>
2021-02-23 17:23:53 -0300

    Commit inicial

---
+++ Vetor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EstruturaDeDados.Tests;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ArrayTest.Show();
+            LinkedListTest.Show();
+        }
+    }
+}
