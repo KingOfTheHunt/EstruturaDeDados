516a642906987cc08329e0354f2e06ef7f599f67
Davi <davifrancisco214@outlook.com>
2021-02-23 17:28:02 -0300

    Estrutura de Dados: LinkedList

---
+++ Vetor/Util/LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace EstruturaDeDados.Util
+{
+    class LinkedList
+    {
+        // Aponta para o primeiro elemento da lista.
+        private Node _head;
+
+        public void AddAtTheBeginning(double value)
+        {
+            Node node = new Node();
+            // Adicionado o valor ao node.
+            node.Value = value;
+            // Apontando para o próximo elemento da lista.
+            node.NextNode = _head;
+            // Fazendo a cabaça apontar para o novo node que foi criado.
+            _head = node;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("[ ");
+
+            // Recebe a referência para o primeiro elemento da lista.
+            Node current = _head;
+
+            while (current != null)
+            {
+                builder.Append(current.Value + " ");
+                // Recebe a referência para o próximo elemento da lista.
+                current = current.NextNode;
+            }
+
+            builder.Append("]");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Vetor/Util/Node.cs
@@ -0,0 +1,11 @@
+﻿namespace EstruturaDeDados.Util
+{
+    // Representa um nó da listaa
+    public class Node
+    {
+        // O valor do nó
+        public double Value { get; set; }
+        // Referência para o próximo nó da da lista.
+        public Node NextNode { get; set; }
+    }
+}
