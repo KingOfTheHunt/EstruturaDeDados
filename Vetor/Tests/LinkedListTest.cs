6d9d419f7d20a866ad11238c2d5b49d717629fff
Davi <davifrancisco214@outlook.com>
2021-02-23 17:28:54 -0300

    Testando a insercao e exibicao do LinkedList

---
+++ Vetor/Tests/LinkedListTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using EstruturaDeDados.Util;
+
+namespace EstruturaDeDados.Tests
+{
+    static class LinkedListTest
+    {
+        public static void Show()
+        {
+            LinkedList linkedList = new LinkedList();
+
+            char op;
+
+            do
+            {
+                Menu();
+                op = Console.ReadLine()[0];
+
+                switch (op)
+                {
+                    case '1':
+                        AddElement(linkedList);
+                        break;
+                    case '2':
+                        Console.WriteLine(linkedList);
+                        break;
+                    case '3':
+                        Console.WriteLine("Fim do programa!");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+
+            } while (op != '3');
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t\tMenu");
+            Console.WriteLine("1 - Adicionar um elemento a lista.");
+            Console.WriteLine("2 - Mostrar a lista.");
+            Console.WriteLine("3 - Sair");
+            Console.Write("Opção: ");
+        }
+
+        private static void AddElement(LinkedList linkedList)
+        {
+            Console.WriteLine();
+            Console.Write("Digite um número: ");
+            double num = double.Parse(Console.ReadLine());
+            linkedList.AddAtTheBeginning(num);
+        }
+    }
+}
